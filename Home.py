62e6e3f79af02c6cd54bfa35a728e9b5521f62d2
Haron <haron@pc-218.home>
2025-10-28 23:38:53 +0100

    Push complet du projet local vers GitHub

--- Home.py
+++ Home.py
@@ -337,12 +337,10 @@ st.success(" N’hésitez pas à me contacter pour échanger sur mes projets ou 
 
 st.title("Mon CV")
 
-    # Chemin du PDF
 pdf_file = "assets/CV_Haron_MATAOUI.pdf"
 
 with open(pdf_file, "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 st.markdown(pdf_display, unsafe_allow_html=True)
-
